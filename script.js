bf5a751765adc8be088e9534c919e10f49674637
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-12-09 14:52:13 +0100

    fetch update but not fixed

--- script.js
+++ script.js
@@ -1,53 +1,50 @@
 const inputField = document.getElementById("searchInput");
 const submitBtn = document.getElementById("submit-button");
+
 const dataIpAdress = document.querySelector("[data-ip]");
 const dataLocation = document.querySelector("[data-location]");
 const dataTimezone = document.querySelector("[data-timezone]");
 const dataIsp = document.querySelector("[data-isp]");
 
-// function request(e) {
-//     e.preventDefault()
-//     getInputEntries()    
-// }
+const bypass_cors_url = 'https://cors-anywhere.herokuapp.com/';
+const api_uri = 'https://geo.ipify.org/api/';
+const current_verion = 'v1';
+const secret_api = 'ENTER_YOUR_API';
 
-// function getInputEntries() {    
-//     console.log(inputValue)
-// }
-
-// function displayOutputvalues() {
-
-// }
-
-class FetchThisOne {
-    constructor(ip, location, isp) {        
-        this.ip = ip
-        this.location = location
-        this.isp = isp        
+const headers_option = {
+    headers: {
+        'Access-Control-Allow-Origin': '*',
     }
-
-    request(url) {        
-        console.log(url)
-        fetch(url) //créer fonction pour ajouter https:// avant chaine de caractère
-        .then(function(res) {
-            if(res.ok) {
-                return res.json();
-            }
-        })
-        .then(function(value) {
-            console.log(value)
-        })
-        .catch(function(err) {
-            console.log('error')
-        })
-    }    
 }
 
-// const url = inputField.value
+function getIpInfo(urlToFetch) {      
+    
+    if(urlToFetch == undefined) {
+        var ipUrl = `${bypass_cors_url}${api_uri}${current_verion}?apiKey=${secret_api}`
+    } else {
+        var ipUrl = `${bypass_cors_url}${api_uri}${current_verion}?apiKey=${secret_api}&ipAdress=${urlToFetch}`
+    }
 
-const urlToFetch = new FetchThisOne(inputField.value)
+    fetch(ipUrl, headers_option)
+    .then(function(res) {
+        console.log(ipUrl)
+        return res.json()
+    })
+    .then(function(data) {
+        console.log(data.ip)        
+    })
+    .catch(function(error) {
+        console.log(error)
+    })
+}
 
 submitBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    urlToFetch.request(inputField.value)
+    e.preventDefault()    
+    getIpInfo(inputField.value)
+    return
 })
+
+
+
+
 
