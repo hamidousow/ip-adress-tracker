ce12931d2342d17706f1b363fdf05e8e96f75c87
Hamidou.S <86967645+hamidousow@users.noreply.github.com>
2021-12-10 11:41:41 +0100

    responsive fixed and fetch with ip works

--- script.js
+++ script.js
@@ -7,13 +7,7 @@ const dataTimezone = document.querySelector("[data-timezone]");
 const dataIsp = document.querySelector("[data-isp]");
 
 const bypass_cors_url = 'https://cors-anywhere.herokuapp.com/';
-
-const apiUrl = "https://geo.ipify.org/api/v2/country?apiKey=at_gbpt1y9UojHQQl8zo1L5mOxMGDgDd&ipAddress=";
-
-// const apiUrl = 'https://geo.ipify.org/api/';
-const version = 'v1';
-
-const secret_api = 'ENTER_YOUR_API';
+const apiRequest = 'https://geo.ipify.org/api/v2/country,city?apiKey=at_gbpt1y9UojHQQl8zo1L5mOxMGDgDd&ipAddress=8.8.8.8';
 
 const headers_option = {
     headers: {
@@ -21,35 +15,54 @@ const headers_option = {
     }
 }
 
-function getIpInfo(urlToFetch) {      
+getIpInfo = (urlToFetch) => {      
     
     if(urlToFetch == undefined) {
-        var ipUrl = `${bypass_cors_url}${apiUrl}`
+        var ipUrl = `${bypass_cors_url}${apiRequest}`
     } else {
-        var ipUrl = `${bypass_cors_url}${apiUrl}${urlToFetch}`
+        var ipUrl = `${bypass_cors_url}${apiRequest}&ipAddress=${urlToFetch}`
     }
 
     fetch(ipUrl, headers_option)
-    .then(function(res) {
-        console.log(ipUrl)
-        return res.json()
-    })
-    .then(function(value) {
+    .then(results => results.json())
+    .then(value  => {
         console.log(value) 
         dataIpAdress.innerHTML = value.ip;
         dataLocation.innerHTML = `${value.location.region}, ${value.location.country}`
         dataTimezone.innerHTML = value.location.timezone;
         dataIsp.innerHTML = value.isp;       
     })
-    .catch(function(error) {
+    .catch(error => {
         console.log(error)
     })
 }
 
+//localisation marker 
+
+const map = L.map('container-map', {
+    'center': [0,0],    
+    'zoom': 25,
+    'layers': [
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        })
+    ]    
+})
+
+updateMarker = (update_marker = [-25, 25]) => {
+    map.setView(update_marker, 13); //first longitude & latitude, second paramater is the zoom level
+    L.marker(update_marker).addTo(map);
+}
+
+// end localisation
+
 submitBtn.addEventListener('click', (e) => {
-    e.preventDefault()    
-    getIpInfo(inputField.value)
-    return
+    e.preventDefault()   
+    if(inputField.value != '' && inputField.value != null) {
+        getIpInfo(inputField.value)
+    } else {
+        alert('veuillez saisir une url ou un ip valide')
+    }   
 })
 
 
